26fb4e58214c1678667dfa00d4d9612d5a1f6277
niubinbin2030 <niubinbin2030@gamil.com>
2015-04-20 15:37:14 +0800

    Signed-off-by: niubinbin2030 <niubinbin2030@gamil.com>

--- src/com/example/my_demo/MainActivity.java
+++ src/com/example/my_demo/MainActivity.java
@@ -4,14 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+	private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtn = (Button) findViewById(R.id.main_btn);
+        mBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
     }
 
 
